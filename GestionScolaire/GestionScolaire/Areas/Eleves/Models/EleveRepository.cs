a01e78fad7ca2304be2794c28e863b7d5c0bc23b
Le Balch Virginie <virginie.lebalch@gmail.com>
2014-12-23 14:08:13 +0100

    ajout du repo de eleve

---
+++ GestionScolaire/GestionScolaire/Areas/Eleves/Models/EleveRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GestionScolaire.Models;
+
+namespace GestionScolaire.Areas.Eleves.Models
+{
+    public class EleveRepository : IDisposable
+    {
+        private Entities data;
+
+        #region Construct
+
+        public EleveRepository()
+        {
+            data = new Entities();
+        }
+
+        public EleveRepository(Entities data)
+        {
+            this.data = data;
+        }
+
+        #endregion
+
+        #region Queries
+
+        public IQueryable<Tutors> All()
+        {
+            return data.Tutors;
+        }
+
+        public Tutors GetPupilById(Guid id)
+        {
+            return All().SingleOrDefault(Tutor => Tutor.Id == id);
+        }
+        #endregion
+
+        #region CRUD
+
+        public void Add(Tutors s)
+        {
+            data.Tutors.Add(s);
+        }
+
+        public void Delete(Tutors s)
+        {
+            data.Tutors.Remove(s);
+        }
+
+        public void DeleteById(Guid id)
+        {
+            data.Tutors.Remove(GetPupilById(id));
+        }
+
+        #endregion
+
+        public void Save()
+        {
+            data.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
